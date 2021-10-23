be9eba861155ff2e6dd52c45f70e4f0291014963
patrickliu022 <patrickliu022@gmail.com>
2021-10-23 13:06:37 -0700

    loaded in environment variables

---
+++ social-y/src/Landing.js
@@ -0,0 +1,19 @@
+import './Landing.css';
+
+function Landing () {
+
+    return (
+        <div>
+            <div>
+                <p>LOGO</p>
+                <h1>Social-y</h1>
+            </div>
+            <div>
+                <p>Log-in</p>
+                <form>
+
+                </form>
+            </div>
+        </div>
+    )
+}
--- social-y/src/index.js
+++ social-y/src/index.js
@@ -1,15 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import dotenv from 'dotenv';
 import reportWebVitals from './reportWebVitals';
 
+dotenv.config();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SEND_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MSMNT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
